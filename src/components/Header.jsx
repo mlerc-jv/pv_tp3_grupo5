8de3b7566faf1c36f37c8d97bfd5ea1fb575d70a
mlerc-jv <jimenavirmeru@gmail.com>
2026-05-11 16:54:31 -0300

    Agrego Header.jsx - Meruvia Jimena

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import "../css/header.css"
+
+const Header =() => {
+    return(
+        <header className="header">
+            <div className="texto">
+                <h1>Gestion de Proyectos Educativos</h1>
+                <p>Bienvenidos a la plataforma "Proyectos Educativos", <br />
+                    en esta podras visualizar tus tareas, proyectos en curso y terminados.<br />
+                    Podras comunicarte con profesores y compañeros
+                </p>            
+            </div>
+           
+        </header>
+    );
+};
+export default Header;
