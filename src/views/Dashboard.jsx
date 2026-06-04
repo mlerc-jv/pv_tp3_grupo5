576b17728fdcade81ff7e70716482b58ecaac2b1
Guadalupe Quispe <guada.bqm@gmail.com>
2026-06-04 01:34:44 -0300

    Implementación Dashboard con Material UI - Quispe Guadalupe

---
+++ src/views/Dashboard.jsx
@@ -0,0 +1,103 @@
+import {
+  Container,
+  Typography,
+  Grid,
+  Card,
+  CardContent
+} from "@mui/material";
+
+import proyectoService from "../services/proyectoService";
+
+function Dashboard() {
+
+  const proyectos = proyectoService.obtenerProyectos();
+
+  const total = proyectos.length;
+
+  const activos = proyectos.filter(
+    proyecto => proyecto.estado === "Activo"
+  ).length;
+
+  const pendientes = proyectos.filter(
+    proyecto => proyecto.estado === "Pendiente"
+  ).length;
+
+  const finalizados = proyectos.filter(
+    proyecto => proyecto.estado === "Finalizado"
+  ).length;
+
+  return (
+    <Container sx={{ mt: 4 }}>
+      <Typography variant="h4" gutterBottom>
+        Dashboard
+      </Typography>
+
+      <Typography variant="body1" sx={{ mb: 4 }}>
+        Bienvenido al sistema de gestión de proyectos educativos.
+        Aquí podrás consultar información general sobre los proyectos registrados.
+      </Typography>
+
+      <Grid container spacing={3}>
+
+        <Grid size={{ xs: 12, md: 3 }}>
+          <Card>
+            <CardContent>
+              <Typography variant="h6">
+                Total de proyectos
+              </Typography>
+
+              <Typography variant="h4">
+                {total}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 3 }}>
+          <Card>
+            <CardContent>
+              <Typography variant="h6">
+                Activos
+              </Typography>
+
+              <Typography variant="h4">
+                {activos}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 3 }}>
+          <Card>
+            <CardContent>
+              <Typography variant="h6">
+                Pendientes
+              </Typography>
+
+              <Typography variant="h4">
+                {pendientes}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 3 }}>
+          <Card>
+            <CardContent>
+              <Typography variant="h6">
+                Finalizados
+              </Typography>
+
+              <Typography variant="h4">
+                {finalizados}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+
+      </Grid>
+    </Container>
+  );
+}
+
+export default Dashboard;
